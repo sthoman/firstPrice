aac95e13dc0a1a9da323cd94346d8dad61d7964f
Sean Thoman <sean@sthoman.com>
2019-01-05 14:55:52 -0500

    fix ecrecover

--- contracts/ECRecover.sol
+++ contracts/ECRecover.sol
@@ -29,9 +29,9 @@ contract ECRecover
     uint8 v;
 
     assembly {
-      r := mload(add(sig, 32))
-      s := mload(add(sig, 64))
-      v := and(mload(add(sig, 65)), 255)
+      r := mload(add(signature, 32))
+      s := mload(add(signature, 64))
+      v := and(mload(add(signature, 65)), 255)
     }
 
     if (v < 27) v += 27;
