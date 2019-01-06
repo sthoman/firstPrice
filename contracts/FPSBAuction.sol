353ca514bd1163e07ee43051e8d101f14f010f80
Sean Thoman <sean@sthoman.com>
2019-01-06 02:36:55 -0500

    Clean up

--- contracts/FPSBAuction.sol
+++ contracts/FPSBAuction.sol
@@ -75,7 +75,8 @@ contract FPSBAuction is
     // is hashed by the bidder before submitting to this function. The
     // hash can be validated during the reveal phase by ecrecover.
     function commit(bytes32 bid, bytes signature)
-      public returns (address)
+      public
+      //returns (address)
     {
       address senderAddress = ecr(bid, signature);
       require(
@@ -87,7 +88,7 @@ contract FPSBAuction is
     //  );
       bidders[senderAddress] = BidderDetails(0, bid, 0, false, true);
     //  commitCount++;
-      return senderAddress;
+    //  return senderAddress;
     }
 
     // Reveal the salt used to hash each bid as well as the actual bid
--- middleware/app/__init__.py
+++ middleware/app/__init__.py
@@ -1,4 +1,5 @@
 import json
+import os
 from flask import Flask, Response, request, jsonify
 from marshmallow import Schema, fields, ValidationError
 from web3 import Web3
@@ -15,29 +16,23 @@ w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
 app = Flask(__name__)
 
 
-@app.route("/auction/commit", methods=['POST'])
+@app.route("/auction/commit-test", methods=['POST'])
 def transaction():
 
     w3.eth.defaultAccount = w3.eth.accounts[1]
-    with open("./app/fpsbAuction.json", 'r') as f:
+
+    dir = os.path.dirname(__file__)
+    path = os.path.join(dir, 'contracts/FPSBAuction.json')
+
+    with open(path, 'r') as f:
         datastore = json.load(f)
     abi = datastore["abi"]
-    #contract_address = datastore["contract_address"]
-
-    # Create the contract instance with the newly-deployed address
-    #body = request.get_json()
-    print(request.form)
-
-    #result, error = BidSchema().load(body)
-    #if error:
-    #    return jsonify(error), 422
 
     address = w3.toChecksumAddress(request.form['address'])
     auction = w3.eth.contract(
         address=address, abi=abi,
     )
 
-    # Commitment scheme
     tx_account = w3.eth.account.create('commit test account now')
     tx_private_key = tx_account.privateKey;
     msg = request.form['bid']
