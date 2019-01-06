c08c6004e3eaecb8c1a9d8119eadcf0c9d617316
Sean Thoman <sean@sthoman.com>
2019-01-06 13:21:28 -0500

    deployment improvements

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,7 +5,7 @@ var fpsbAuctionContract = artifacts.require("./FPSBAuction.sol");
 //TODO create auction contract registry to manage contract deployment
 module.exports = function(deployer) {
   deployer.deploy(fpsbAuctionContract).then(function() {
-    fs.writeFile("address.txt", fpsbAuctionContract.address, function(err) {
+      fs.writeFile("address.txt", fpsbAuctionContract.address, function(err) {
     });
   });
 };
