b58a2b1301d1b697bdc2e69b88fef251420d6a7f
Sean Thoman <sean@sthoman.com>
2019-01-06 02:54:12 -0500

    More clean up

--- middleware/app/routes.py
+++
@@ -1,8 +0,0 @@
-from app import app
-from web3 import Web3
-from flask import render_template
-
-@app.route('/')
-def index():
-    """Serve the index HTML"""
-    return render_template('index.html')
--- middleware/app/storage.py
+++
@@ -1,6 +0,0 @@
-import redis
-
-def store(key, value):
-  r = redis.Redis('localhost')
-  retv = r.set(key, value)
-  return retv
