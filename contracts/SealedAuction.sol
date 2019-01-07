56ec581e0ba90808383bbae07165561d9bdce9e4
Sean Thoman <sean@sthoman.com>
2019-01-07 12:14:13 -0500

    updated SealedAuction contract

--- contracts/SealedAuction.sol
+++ contracts/SealedAuction.sol
@@ -17,80 +17,71 @@
 */
 pragma solidity 0.4.24;
 
+import "./ECRecover.sol";
+
 // A basic prototype SealedAuction contract, just parts of a basic commit scheme ensuring
 // auction participants are not aware of each others' bids during the commit phase of the
 // auction.
 //
-contract SealedAuction
+contract SealedAuction is ECRecover
 {
-    struct AuctionDetails {
-        uint256 beginTimeSeconds;
-        uint256 endTimeSeconds;
-        uint256 beginRevealTimeSeconds;
-        uint256 currentTimeSeconds;
-        uint256 reservePrice;
+  struct BidderDetails {
+      uint256 amount;
+      uint256 salt;
+      bytes32 bid;
+      bool revealed;
+      bool committed;
+  }
+
+  // highest bid
+  uint256 commitCount;
+  uint256 revealCount;
+  uint256 highestBid;
+  address highestBidder;
+
+  // bidders
+  mapping(address => BidderDetails) public bidders;
+
+  // Add a commitment, also known as a bid in this auction. Each bid
+  // is hashed by the bidder before submitting to this function. The
+  // hash can be validated during the reveal phase by ecrecover.
+  function commit(bytes32 bid, bytes memory signature)
+    public
+  {
+    address senderAddress = ecr(bid, signature);
+    require(
+          senderAddress != address(0), "INVALID_ECRECOVER"
+    );
+    require(
+          bidders[senderAddress].committed == false, "INVALID_COMMIT_UNIQUENESS"
+    );
+    bidders[senderAddress] = BidderDetails(0, 0, bid, false, true);
+    commitCount++;
+  }
+
+  // Reveal the salt used to hash each bid as well as the actual bid
+  // amount after the auction is closed. This is analogous to opening
+  // a sealed envelope containing each bidders' bid amount. When the
+  // auction is over this contract can determine the highest bid.
+  function reveal(uint256 amount, string salt, bytes memory signature)
+    public
+  {
+    // Revealing a commitment to a previous bid requires the sender
+    // to provide their salt and the actual bid amount.
+    bytes32 hashed = keccak256(abi.encodePacked(amount, salt));
+    address sender = ecr(hashed, signature);
+    require(
+        bidders[sender].bid == hashed,
+          "INVALID_REVEAL"
+    );
+    //TODO requires for auction state, also sender amount should not be string ultimately
+    bidders[sender].revealed = true;
+    bidders[sender].amount = amount;
+
+    if (bidders[sender].amount > highestBid) {
+      highestBid = bidders[sender].amount;
+      highestBidder = sender;
     }
-
-    struct BidderDetails {
-        uint256 salt;
-        bytes32 bid;
-        uint256 amount;
-        bool revealed;
-    }
-
-    // highest bid
-    uint256 commitCount;
-    uint256 revealCount;
-    uint256 highestBid;
-    address highestBidder;
-
-    // bidders
-    mapping(address => BidderDetails) public bidders;
-
-    //
-    constructor() public { }
-
-    // Add a commitment, also known as a bid in this auction. Each bid
-    // is hashed by the bidder before submitting to this function. The
-    // hash can later be validated during the reveal phase.
-    function addCommit(bytes32 bid)
-      public
-    {
-      require(
-            bidders[msg.sender].bid == bid,
-            "INVALID_COMMIT_UNIQUENESS"
-      );
-      bidders[msg.sender] = BidderDetails(0, bid, 0, false);
-      commitCount++;
-    }
-
-    // Reveal the salt used to hash each bid as well as the actual bid
-    // amount after the auction is closed. This is analogous to opening
-    // a sealed envelope containing each bidders' bid amount. When the
-    // auction is over this contract can determine the highest bid.
-    //
-    function addReveal(bytes32 salt, uint256 amount)
-      public
-    {
-      // Revealing a commitment to a previous bid requires the sender
-      // to provide their salt and the actual bid amount. It should not
-      // already be revealed
-      require(
-          bidders[msg.sender].revealed == true,
-            "INVALID_REVEAL_UNIQUENESS"
-      );
-      require(
-          bidders[msg.sender].bid == keccak256(abi.encodePacked(amount, salt)),
-            "INVALID_REVEAL"
-      );
-      //TODO requires for auction state
-      bidders[msg.sender].revealed = true;
-      bidders[msg.sender].amount = amount;
-      //
-      if (bidders[msg.sender].amount > highestBid) {
-        highestBid = bidders[msg.sender].amount;
-        highestBidder = msg.sender;
-      }
-      revealCount++;
-    }
+    revealCount++;
+  }
 }
